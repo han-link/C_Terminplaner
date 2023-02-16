1bbb76888f0ebf4ce5d2c77c13ea3185c8426860
han-link <115926359+han-link@users.noreply.github.com>
2023-02-16 17:40:30 +0100

    removed unnessasary comments, @bug_fixed structured list in createList(),@feature print Date-Time-String in debugList(), @bug_fixed in insertElement()

--- scheduler.c
+++ scheduler.c
@@ -1,35 +1,21 @@
-/*
-Compiler command:
-    gcc scheduler.c -o scheduler -Wall -pedantic-errors -g
-*/
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
-#include <stdbool.h>
+#include <string.h>
+// #include <stdbool.h>
 
-/*
-    Structure to store the appointment
- */
 typedef struct
 {
     time_t start;
     char *description;
 } Appointment;
 
-/*
-    This structure stores a list element together with a pointer to
-    the successor
- */
 typedef struct Element
 {
     Appointment *appointment;
     struct Element *next;
 } Element;
 
-/*
-    This structure stores the beginning and the end of the list
- */
 typedef struct
 {
     Element *head, *tail;
@@ -38,12 +24,23 @@ typedef struct
 void debugList(List *list)
 {
     Element *current = list->head->next;
-    while (current != NULL) {
-        if (current->appointment == NULL) {
+    while (current != NULL)
+    {
+        if (current->appointment == NULL)
+        {
             printf("Error: Invalid appointment data\n");
-        } else {
+        }
+        else if (current->next == NULL)
+        {
+            printf("Reached end of list");
+        }
+        else
+        {
+            char buff[20];
+            time_t start_time = current->appointment->start;
+            strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&start_time));
             printf("Appointment: %s\n", current->appointment->description);
-            printf("Start time: %ld\n", current->appointment->start);
+            printf("Start time: %s\n", buff);
         }
         current = current->next;
     }
@@ -53,45 +50,44 @@ void debugList(List *list)
 List *createList()
 {
     List *list = malloc(sizeof(List));
-    Element *head = malloc(sizeof(Element));
-    Element *tail = malloc(sizeof(Element));
+    list->head = malloc(sizeof(Element));
+    list->tail = malloc(sizeof(Element));
 
-    list->head = head;
-    list->tail = tail;
-    head->next = tail;
-    tail->next = NULL;
+    list->head->next = list->tail;
+    list->tail->next = NULL;
 
     return list;
 }
 
-void clearList(List list)
+/*void clearList(List list)
 {
          
-}
+}*/
 
 void insertElement(List *list, time_t time, const char *text)
 {
     // create a new Appointment struct and assign its fields
     Appointment *new_appointment = malloc(sizeof(Appointment));
     new_appointment->start = time;
-    new_appointment->description = text;
+    new_appointment->description = malloc(strlen(text) + 1);
+    strcpy(new_appointment->description, text);
 
     // create a new Element struct and assign its fields
     Element *new_element = malloc(sizeof(Element));
     new_element->appointment = new_appointment;
 
     // insert the new element into the list
-    if (list->head == NULL)
+    if (list->head->next == list->tail)
     {
         // if the list is empty, set the head and tail pointers to the new element
-        new_element->next = list->tail;
         list->head->next = new_element;
+        new_element->next = list->tail;
     } 
     else 
     {
         // otherwise, find the correct position to insert the new element
-        Element *current = list->head->next;
-        while (current->next != NULL && current->next->appointment->start <= time) 
+        Element *current = list->head;
+        while (current->next != list->tail && current->next->appointment->start <= time)
         {
             current = current->next;
         }
@@ -102,7 +98,7 @@ void insertElement(List *list, time_t time, const char *text)
 }
 
 
-void /* should return a pointer */ findElement(List list, const char *searchText)
+/*void findElement(List list, const char *searchText)
 {
 }
 
@@ -117,13 +113,16 @@ void printAppointment(Appointment p)
 
 void printList(List list, int day, int month, int year)
 {
-}
+}*/
 
 int main(void)
 {
     List *test_list = createList();
-    insertElement(test_list, 1676363073, "Erstes Element");
-    // insertElement(test_list, 1676363114, "Zweites Element");
-    // insertElement(test_list, 1676363165, "Drittes Element");
+    insertElement(test_list, 1676363073, "First Element");
+    insertElement(test_list, 1676363114, "Second Element");
+    insertElement(test_list, 1676363165, "Third Element");
+    insertElement(test_list, 1673880593, "Fourth Element");
+    insertElement(test_list, 1452955793, "Old Event");
+    insertElement(test_list, 1452955793, "Old Event 2");
     debugList(test_list);
 }
